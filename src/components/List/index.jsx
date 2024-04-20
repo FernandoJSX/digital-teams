a096ee381447d8f2d13bee756fd82f567bc3d2ba
FernandoJSX <fernandojsx@gmail.com>
2024-04-20 16:54:58 -0300

    Formulários para cadastro de time e participante.

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -2,8 +2,11 @@ import { Card } from "primereact/card";
 import { Button } from "primereact/button";
 import { Sidebar } from 'primereact/sidebar';
 import { Dialog } from 'primereact/dialog';
-import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog'; // For <ConfirmDialog /> component
+import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog';
 import { useEffect, useState } from "react";
+import { InputText } from 'primereact/inputtext';
+import { InputMask } from 'primereact/inputmask';
+import { useForm } from "react-hook-form";
 
 const List = () => {
 
@@ -12,26 +15,51 @@ const List = () => {
     const [mostrarDialog, setMostrarDialog] = useState(false);
     const [teams, setTeams] = useState([]);
 
-    function confirmacao() {
+    const { register, handleSubmit, reset } = useForm();
+    const { register: registerP, handleSubmit: handleSubmitP, reset: resetP } = useForm();
+
+    async function cadastrar(dados) {
+        const request = await fetch('http://localhost:3000/teams', {
+            method: 'post',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(dados)
+        })
+        const response = await request.json();
+
+        if (response) {
+            reset();
+            setMostrarSidebar(false);
+            buscarTeams();
+        }
+    }
+
+    function addParticipante(dados) {
+
+    }
+
+    function confirmacao(id) {
         confirmDialog({
             header: "Aviso:",
             message: "Deseja realmente apagar esse item?",
             acceptLabel: "Sim",
             rejectLabel: "Não",
-            accept: () => {
-                alert('Confirmou')
+            accept: async () => {
+                await fetch(`http://localhost:3000/teams/${id}`, {
+                    method: 'delete'
+                }).finally(() => {
+                    buscarTeams();
+                });
             },
-            reject: () => {
-                alert('Cancelou')
-            }
-        })
+        });
     }
 
     async function buscarTeams() {
         const request = await fetch("http://localhost:3000/teams");
         const response = await request.json();
         setTeams(response);
-        
+
         console.log(teams);
     }
 
@@ -39,16 +67,16 @@ const List = () => {
         buscarTeams();
     }, []);
 
-    const titulo = (
+    const titulo = (nome) => (
         <div className="flex justify-content-between align-items-center text-sm">
-            titulo do card
+            {nome}
             <i className="pi pi-eye" onClick={() => setMostrarDialog(true)} ></i>
         </div>
     );
-    const footer = (
+    const footer = (id) => (
         <div className="flex gap-3">
             <Button label="Adicionar" className="flex-1 px-0" onClick={() => setMostrarSidebarAdd(true)} />
-            <Button icon="pi pi-trash" onClick={confirmacao} />
+            <Button icon="pi pi-trash" onClick={() => confirmacao(id)} />
         </div>
     );
     return (
@@ -62,36 +90,65 @@ const List = () => {
                 />
             </h2>
 
-            <Card style={{ width: `calc(20% - 13px)` }} title={titulo} footer={footer}>
-                <h1 className="mx-auto flex flex-column text-center">0<span className="text-sm">/ 0</span></h1>
-            </Card>
-            <Card style={{ width: `calc(20% - 13px)` }} title={titulo} footer={footer}>
-                <h1 className="mx-auto flex flex-column text-center">0<span className="text-sm">/ 0</span></h1>
-            </Card>
-            <Card style={{ width: `calc(20% - 13px)` }} title={titulo} footer={footer}>
-                <h1 className="mx-auto flex flex-column text-center">0<span className="text-sm">/ 0</span></h1>
-            </Card>
-            <Card style={{ width: `calc(20% - 13px)` }} title={titulo} footer={footer}>
-                <h1 className="mx-auto flex flex-column text-center">0<span className="text-sm">/ 0</span></h1>
-            </Card>
-            <Card style={{ width: `calc(20% - 13px)` }} title={titulo} footer={footer}>
-                <h1 className="mx-auto flex flex-column text-center">0<span className="text-sm">/ 0</span></h1>
-            </Card>
+            {
+                teams && teams.map((team) => (
+
+                    <Card
+                        key={`team${team.id}`}
+                        style={{ width: `calc(20% - 13px)` }}
+                        title={titulo(team.nome)}
+                        footer={footer(team.id)}
+                    >
+                        <h1 className="mx-auto flex flex-column text-center">{team.participantes.length}<span className="text-sm">/ {team.capacidade}</span></h1>
+                    </Card>
+                ))
+            }
 
             <Sidebar
                 visible={mostrarSidebar}
                 onHide={() => setMostrarSidebar(false)}
-                position="left"
+                position="right"
             >
-                Teste
+                <form onSubmit={handleSubmit(cadastrar)}>
+                    <h3>Cadastrar</h3>
+                    <label htmlFor="nome" className="uppercase text-sm font-bold mb-2">Nome</label>
+                    <InputText
+                        id="nome"
+                        placeholder="Digite o nome do time"
+                        className="w-full mb-3"
+                        {...register('nome', { required: true })}
+
+                    />
+                    <label htmlFor="capacidade" className="uppercase text-sm font-bold mb-2 block">Capacidade</label>
+                    <InputMask
+                        id="capacidade"
+                        mask={"99"}
+                        className="w-full mb-3"
+                        {...register('capacidade', { required: true })}
+                    />
+                    <Button label="Criar" className="w-full" />
+                </form>
+
             </Sidebar>
 
             <Sidebar
                 visible={mostrarSidebarAdd}
                 onHide={() => setMostrarSidebarAdd(false)}
-                position="right"
+                position="left"
             >
-                Teste Add
+                <form onSubmit={handleSubmitP(addParticipante)}>
+                    <h3>Cadastrar</h3>
+                    <label htmlFor="nome" className="uppercase text-sm font-bold mb-2">Nome</label>
+                    <InputText
+                        id="nome"
+                        placeholder="Digite o nome do participante"
+                        className="w-full mb-3"
+                        {...registerP('nome', { required: true })}
+
+                    />
+                    <Button label="Criar" className="w-full" />
+                </form>
+
             </Sidebar>
 
             <Dialog
